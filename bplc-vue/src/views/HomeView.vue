1513c36c3cfadb7d44359bdd6b20b09c51f9ef86
Mingkkong <bmg1203@naver.com>
2022-05-26 13:36:12 +0900

    commit homeview

--- bplc-vue/src/views/HomeView.vue
+++ bplc-vue/src/views/HomeView.vue
@@ -1,5 +1,28 @@
 <template>
-  
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="@/assets/main/m1.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="@/assets/main/m2.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="@/assets/main/m3.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="@/assets/main/m4.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 </template>
 
 <script lang="ts">
@@ -7,9 +30,14 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'HomeView',
-  
+
 });
 </script>
 
 <style scoped>
+#carouselExampleControls {
+  padding: 90px 0px 0px 0px;
+  width: 100%;
+  overflow: hidden;
+}
 </style>
